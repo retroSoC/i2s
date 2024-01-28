c42a643a3f078a189c666ae4b7ef92572df5489a
maksyuki <maksyuki@126.com>
2024-01-28 17:00:03 +0800

    feat: add and modify reg def

--- rtl/apb4_i2s.sv
+++ rtl/apb4_i2s.sv
@@ -20,4 +20,72 @@ module apb4_i2s #(
     i2s_if.dut    i2s
 );
 
+  logic [3:0] s_apb4_addr;
+  logic s_apb4_wr_hdshk, s_apb4_rd_hdshk;
+  logic [`I2S_CTRL_WIDTH-1:0] s_i2s_ctrl_d, s_i2s_ctrl_q;
+  logic s_i2s_ctrl_en;
+  logic [`I2S_DIV_WIDTH-1:0] s_i2s_div_d, s_i2s_div_q;
+  logic s_i2s_div_en;
+  logic [`I2S_STAT_WIDTH-1:0] s_i2s_stat_d, s_i2s_stat_q;
+  // bit
+  logic s_bit_en, s_bit_txie, s_bit_rxie, s_bit_clr, s_bit_msr;
+  logic s_bit_pol, s_bit_lsb, s_bit_chl;
+  logic [1:0] s_bit_wm, s_bit_fmt, s_bit_chm, s_bit_dal;
+  logic [4:0] s_bit_txth, s_bit_rxth;
+  logic s_bit_txif, s_bit_rxif, s_busy;
+
+  assign s_apb4_addr     = apb4.paddr[5:2];
+  assign s_apb4_wr_hdshk = apb4.psel && apb4.penable && apb4.pwrite;
+  assign s_apb4_rd_hdshk = apb4.psel && apb4.penable && (~apb4.pwrite);
+  assign apb4.pready     = 1'b1;
+  assign apb4.pslverr    = 1'b0;
+
+  assign s_bit_en        = s_i2s_ctrl_q[0];
+  assign s_bit_txie      = s_i2s_ctrl_q[1];
+  assign s_bit_rxie      = s_i2s_ctrl_q[2];
+  assign s_bit_clr       = s_i2s_ctrl_q[3];
+  assign s_bit_msr       = s_i2s_ctrl_q[4];
+  assign s_bit_pol       = s_i2s_ctrl_q[5];
+  assign s_bit_lsb       = s_i2s_ctrl_q[6];
+  assign s_bit_wm        = s_i2s_ctrl_q[8:7];
+  assign s_bit_fmt       = s_i2s_ctrl_q[10:9];
+  assign s_bit_chm       = s_i2s_ctrl_q[12:11];
+  assign s_bit_chl       = s_i2s_ctrl_q[13];
+  assign s_bit_dal       = s_i2s_ctrl_q[15:14];
+  assign s_bit_txth      = s_i2s_ctrl_q[20:16];
+  assign s_bit_rxth      = s_i2s_ctrl_q[25:21];
+  assign s_bit_txif      = s_i2s_stat_q[0];
+  assign s_bit_rxif      = s_i2s_stat_q[1];
+  assign s_busy          = 1'b0;  // TODO:
+
+  assign s_i2s_ctrl_en   = s_apb4_wr_hdshk && s_apb4_addr == `I2S_CTRL && ~s_busy;
+  assign s_i2s_ctrl_d    = s_i2s_ctrl_en ? apb4.pwdata[`I2S_CTRL_WIDTH-1:0] : s_i2s_ctrl_q;
+  dffer #(`I2S_CTRL_WIDTH) u_i2s_ctrl_dffer (
+      apb4.pclk,
+      apb4.presetn,
+      s_i2s_ctrl_en,
+      s_i2s_ctrl_d,
+      s_i2s_ctrl_q
+  );
+
+  assign s_i2s_div_en = s_apb4_wr_hdshk && s_apb4_addr == `I2S_DIV && ~s_busy;
+  assign s_i2s_div_d  = s_i2s_div_en ? apb4.pwdata[`I2S_DIV_WIDTH-1:0] : s_i2s_div_q;
+  dffer #(`I2S_DIV_WIDTH) u_i2s_div_dffer (
+      apb4.pclk,
+      apb4.presetn,
+      s_i2s_div_en,
+      s_i2s_div_d,
+      s_i2s_div_q
+  );
+
+  always_comb begin
+    apb4.prdata = '0;
+    if (s_apb4_rd_hdshk) begin
+      unique case (s_apb4_addr)
+        `I2S_CTRL: apb4.prdata[`I2S_CTRL_WIDTH-1:0] = s_i2s_ctrl_q;
+        `I2S_DIV:  apb4.prdata[`I2S_DIV_WIDTH-1:0] = s_i2s_div_q;
+        default:   apb4.prdata = '0;
+      endcase
+    end
+  end
 endmodule
--- rtl/i2s_define.sv
+++ rtl/i2s_define.sv
@@ -13,47 +13,47 @@
 
 /* register mapping
  * I2S_CTRL:
- * BITS:   | 31:22 | 21:18 | 17:13 | 12:11 | 10:9 | 8:7 | 6   | 5   | 4   | 3   | 2    | 1    | 0  |
- * FIELDS: | RES   | RXTH  | TXTH  | CH    | FMT  | WM  | LSB | POL | MSR | CLR | RXIE | TXIE | EN |
- * PERMS:  | NONE  | RW    | RW    | RW    | RW   | RW  | RW  | RW  | RW  | RW  | RW   | RW   | RW |
- * ---------------------------------------------------------
- * I2S_PSCR:
+ * BITS:   | 31:26 | 25:21 | 20:16 | 15:14 | 13  | 12:11 | 10:9 | 8:7 | 6   | 5   | 4   | 3   | 2    | 1    | 0  |
+ * FIELDS: | RES   | RXTH  | TXTH  | DAL   | CHL | CHM   | FMT  | WM  | LSB | POL | MSR | CLR | RXIE | TXIE | EN |
+ * PERMS:  | NONE  | RW    | RW    | RW    | RW  | RW    | RW   | RW  | RW  | RW  | RW  | RW  | RW   | RW   | RW |
+ * ---------------------------------------------------------------------------------------------------------------
+ * I2S_DIV:
  * BITS:   | 31:16 | 15:0 |
- * FIELDS: | RES   | PSCR |
+ * FIELDS: | RES   | DIV  |
  * PERMS:  | NONE  | RW   |
- * -----------------------------------
+ * ---------------------------------------------------------------------------------------------------------------
  * I2S_TXR:
  * BITS:   | 31:0   |
  * FIELDS: | TXDATA |
  * PERMS:  | W      |
- * ----------------------------------
+ * ---------------------------------------------------------------------------------------------------------------
  * I2S_RXR:
  * BITS:   | 31:0   |
  * FIELDS: | RXDATA |
  * PERMS:  | R      |
- * ----------------------------------
+ * ---------------------------------------------------------------------------------------------------------------
  * I2S_STAT:
  * BITS:   | 31:5 | 4    | 3    | 2    | 1    | 0    |
  * FIELDS: | RES  | RETY | TFUL | BUSY | RXIF | TXIF |
  * PERMS:  | NONE | R    | R    | R    | R    | R    |
- * ----------------------------------
+ * ---------------------------------------------------------------------------------------------------------------
 */
 
 // verilog_format: off
 `define I2S_CTRL 4'b0000 // BASEADDR + 0x00
-`define I2S_PSCR 4'b0001 // BASEADDR + 0x04
+`define I2S_DIV 4'b0001 // BASEADDR + 0x04
 `define I2S_TXR  4'b0010 // BASEADDR + 0x08
 `define I2S_RXR  4'b0011 // BASEADDR + 0x0C
 `define I2S_STAT 4'b0100 // BASEADDR + 0x10
 
 `define I2S_CTRL_ADDR {26'b00, `I2S_CTRL, 2'b00}
-`define I2S_PSCR_ADDR {26'b00, `I2S_PSCR, 2'b00}
+`define I2S_DIV_ADDR {26'b00, `I2S_DIV, 2'b00}
 `define I2S_TXR_ADDR  {26'b00, `I2S_TXR , 2'b00}
 `define I2S_RXR_ADDR  {26'b00, `I2S_RXR , 2'b00}
 `define I2S_STAT_ADDR {26'b00, `I2S_STAT, 2'b00}
 
-`define I2S_CTRL_WIDTH 22
-`define I2S_PSCR_WIDTH 16
+`define I2S_CTRL_WIDTH 26
+`define I2S_DIV_WIDTH 16
 `define I2S_TXR_WIDTH  32
 `define I2S_RXR_WIDTH  32
 `define I2S_STAT_WIDTH 5
@@ -66,24 +66,62 @@
 `define I2S_FMT_MSB 2'b01
 `define I2S_FMT_LSB 2'b10
 
-`define I2S_CH_STERO 2'b00
-`define I2S_CH_LEFT  2'b01
-`define I2S_CH_RIGHT 2'b10
+`define I2S_CHM_STERO 2'b00
+`define I2S_CHM_LEFT  2'b01
+`define I2S_CHM_RIGHT 2'b10
+
+`define I2S_CHL_16_BITS 1'b0
+`define I2S_CHL_32_BITS 1'b1
+
+`define I2S_DAL_8_BITS  2'b00
+`define I2S_DAL_16_BITS 2'b01
+`define I2S_DAL_24_BITS 2'b10
+`define I2S_DAL_32_BITS 2'b11
 // verilog_format: on
 
 interface i2s_if (
-    logic aud_clk_i,
-    logic aud_rst_n_i
+    input logic aud_clk_i,
+    input logic aud_rst_n_i
 );
   logic mclk_o;
   logic sck_o;
   logic sck_i;
-  logic sck_en_o,
+  logic sck_en_o;
   logic ws_o;
   logic ws_i;
   logic ws_en_o;
   logic sd_o;
   logic sd_i;
   logic irq_o;
+
+  modport dut(
+      input aud_clk_i,
+      input aud_rst_n_i,
+      output mclk_o,
+      output sck_o,
+      input sck_i,
+      output sck_en_o,
+      output ws_o,
+      input ws_i,
+      output ws_en_o,
+      output sd_o,
+      input sd_i,
+      output irq_o
+  );
+
+  modport tb(
+      input aud_clk_i,
+      input aud_rst_n_i,
+      input mclk_o,
+      input sck_o,
+      output sck_i,
+      input sck_en_o,
+      input ws_o,
+      output ws_i,
+      input ws_en_o,
+      input sd_o,
+      output sd_i,
+      input irq_o
+  );
 endinterface
 `endif
